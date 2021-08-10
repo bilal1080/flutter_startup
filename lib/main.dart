48a10c5e339da1e9c17b1ba2d9d0d490ec267571
malikjee760 <85275592+malikjee760@users.noreply.github.com>
2021-08-10 17:00:15 +0500

    wordsgenerator

--- lib/main.dart
+++ lib/main.dart
@@ -4,56 +4,60 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: Text('Random Words Generator'),
         ),
-        body:  Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-              children: <Widget>[
-                SizedBox(height: 20,),
-                Text(questions.elementAt(0)),
-                ElevatedButton(
-                    onPressed: (){
-                      print("1st butoon is pressed");
-                    },
-                    child: Text("Answer"),
-
-                ),
-                SizedBox(height: 20,),
-                Text(questions.elementAt(1)),
-                ElevatedButton(
-                  onPressed: (){
-                    print("2nd butoon is pressed");
-                  },
-                  child: Text("Answer"),
-                ),
-                SizedBox(height: 20,),
-                Text(questions.elementAt(2)),
-                ElevatedButton(
-                  onPressed: (){
-                    print("3rd butoon is pressed");
-                  },
-                  child: Text("Answer"),
-                ),
-
-              ],
-
-            ),
-        ),
-        ),
-
+        body: Randomword(),
+      ),
     );
   }
-  var questions= [
-    'what is your name?',
-    'whats is your hobey?',
-    'what is your religion?',
-  ];
 
+}
+class Randomword extends StatefulWidget {
+  @override
+  _RandomwordState createState() => _RandomwordState();
+}
+
+class _RandomwordState extends State<Randomword> {
+  final _suggestions = <WordPair>[];
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return _buildSuggestions();
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: 20,
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asCamelCase,
+        style: TextStyle(
+          fontSize: 18.0
+        ),
+      ),
+    );
+  }
 }
